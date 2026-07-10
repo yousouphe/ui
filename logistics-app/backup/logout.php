1c7c8f17facb5b88d4b420a60c16a87b54260b40
yousouphe <42108608+yousouphe@users.noreply.github.com>
2026-07-10 09:22:40 +0100

    Delete logistics-app/backup directory

--- logistics-app/backup/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/config/functions.php';
-session_unset();
-session_destroy();
-session_start();
-flash('success', 'Logged out successfully.');
-redirect_to('login.php');
